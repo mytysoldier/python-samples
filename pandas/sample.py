95641b01fa9de3e8176f3e265992dbe9431632a7
mytysoldier <yoshi008t@gmail.com>
2023-08-09 16:39:02 +0900

    update

--- pandas/sample.py
+++ pandas/sample.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-s1 = pd.Series([1,2,3], index=['a','b','c'])
-print(s1)
+df = pd.read_csv("data.csv", encoding='shift_jis', header=[1, 2], index_col=0)
+print(df)
+df.plot()
 
-print(s1['b'])
-print(s1["b"])
+# グラフを画像ファイルに保存
+plt.savefig("output.png")
+
+# 画像ファイルを外部プログラムで表示
+# 例: Windowsでは「explorer」としてファイルを開く
+import subprocess
+subprocess.run(["explorer", "output.png"])
